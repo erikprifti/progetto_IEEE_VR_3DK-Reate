013115862b6bde44b145e5a885d0eafe0a713341
Alessandro Visconti <alessandro.visconti97@gmail.com>
2023-01-11 18:15:19 +0100

    TMP_Text

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -5,29 +5,16 @@ using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
-    public TextMeshPro slot1;
-    public TextMeshPro slot2;
-    public TextMeshPro slot3;
+    public TMP_Text slot1;
+    public TMP_Text slot2;
+    public TMP_Text slot3;
+    public TMP_Text slot4;
 
     private int freeslot = 1;
 
     public void addPlayer(string name)
     {
-        if (freeslot == 1)
-        {
-            freeslot++;
-            slot1.text = name;
-        }
-        if (freeslot == 2)
-        {
-            freeslot++;
-            slot2.text = name;
-        }
-        if (freeslot == 3)
-        {
-            freeslot++;
-            slot3.text = name;
-        }
+        slot1.text = "giocatore " + freeslot;
     }
 
 }
