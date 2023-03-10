9e35a86d9e1a52d72429d8181d012ff4632ab411
Viscoale97 <alessandro.visconti97@gmail.com>
2023-03-10 12:26:37 +0100

    scritto a button

--- Assets/Scripts/cubeSel.cs
+++ Assets/Scripts/cubeSel.cs
@@ -2,10 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class cubeSel : MonoBehaviour
 {
+    public InputActionReference colorReference = null;
+
+    public MeshRenderer meshRenderer = null;
     public XRSimpleInteractable takeScript;
+    public  Material currentColor;
+    public static Material grigio; //0
+    public static Material rosso;  //1
+    public static Material verde;  //2
+    public static Material blu;    //3
+    public static Material rosa;   //4
+    public static Material giallo;  //5
+    public static Material arancione;  //6
+    public static Material viola;  //7
+    public static Material azzurro;  //8
+    public static Material marrone;  //9
+    public int value = 0;
+
+    private Material[] colors = { grigio, rosso, verde, blu, rosa, giallo, arancione, viola, azzurro, marrone };
+
     GameObject parent;
     private bool entered = false;
 
@@ -40,6 +59,8 @@ public class cubeSel : MonoBehaviour
         }
 
 
+        colorReference.action.started += changeColor;
+
     }
 
     public void OnDiselection()
@@ -54,8 +75,21 @@ public class cubeSel : MonoBehaviour
             entered = false;
         }
 
+        colorReference.action.started -= changeColor;
+
         //trigger
-      //  gameObject.GetComponent<BoxCollider>().isTrigger = false;
+        //  gameObject.GetComponent<BoxCollider>().isTrigger = false;
         //   gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    private void changeColor(InputAction.CallbackContext context)
+    {
+       // bool isActive = !gameObject.activeSelf;
+        currentColor = colors[value];
+        meshRenderer.material = currentColor;
+        value++;
+        if(value == 10) { value = 0; }
+
+
     }
 }
